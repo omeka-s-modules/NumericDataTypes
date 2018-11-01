f6b776d1a2c6bf814630a77b522ff6fcbd115910
Jim Safley <jimsafley@gmail.com>
2018-11-01 15:16:42 -0400

    Namespace module-specific JS

--- asset/js/numeric-data-types.js
+++ asset/js/numeric-data-types.js
@@ -1,3 +1,28 @@
+var NumericDataTypes = {
+    /**
+     * Set a date to a value.
+     *
+     * @param v Value input
+     * @param y Year input
+     * @param m Month select
+     * @param d Day input
+     */
+    setDateValue : function(v, y, m, d) {
+        var year = y.val();;
+        var month = m.val();
+        var day = d.val();
+        if (year && month && day) {
+            v.val(`${year}-${month}-${day}`);
+        } else if (year && month) {
+            v.val(`${year}-${month}`);
+        } else if (year) {
+            v.val(year);
+        } else {
+            v.val(null); // must have year
+        }
+    }
+};
+
 $(document).on('o:prepare-value', function(e, type, value) {
     if ('numeric:timestamp' === type) {
         var v = value.find('input[data-value-key="@value"]');
@@ -5,45 +30,20 @@ $(document).on('o:prepare-value', function(e, type, value) {
         var m = value.find('select[name="numeric-timestamp-month"]');
         var d = value.find('input[name="numeric-timestamp-day"]');
         var matches = /(-?\d+)(-(\d{1,2}))?(-(\d{1,2}))?/.exec(v.val());
-
         // Set existing year, month, and day during initial load.
         if (matches) {
             y.val(parseInt(matches[1]));
             m.val(matches[3] ? parseInt(matches[3]) : null);
             d.val(matches[5] ? parseInt(matches[5]) : null);
         }
-
         y.on('input', function(e) {
-            setDateValue(v, y, m, d);
+            NumericDataTypes.setDateValue(v, y, m, d);
         });
         m.on('change', function(e) {
-            setDateValue(v, y, m, d);
+            NumericDataTypes.setDateValue(v, y, m, d);
         });
         d.on('input', function(e) {
-            setDateValue(v, y, m, d);
+            NumericDataTypes.setDateValue(v, y, m, d);
         });
     }
 });
-
-/**
- * Set a date to a value.
- *
- * @param v Value input
- * @param y Year input
- * @param m Month select
- * @param d Day input
- */
-var setDateValue = function (v, y, m, d) {
-    var year = y.val();;
-    var month = m.val();
-    var day = d.val();
-    if (year && month && day) {
-        v.val(`${year}-${month}-${day}`);
-    } else if (year && month) {
-        v.val(`${year}-${month}`);
-    } else if (year) {
-        v.val(year);
-    } else {
-        v.val(null); // must have year
-    }
-}
