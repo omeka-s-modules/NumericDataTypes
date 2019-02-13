ccd5b3093a5903afa20eb359da154986d4dbc56a
Jim Safley <jimsafley@gmail.com>
2019-02-13 13:04:35 -0500

    Add option to disambiguate numeric properties

--- src/Form/Element/NumericPropertySelect.php
+++ src/Form/Element/NumericPropertySelect.php
@@ -35,6 +35,8 @@ class NumericPropertySelect extends Select
     public function getValueOptions()
     {
         $dataTypes = $this->getOption('numeric_data_type');
+        $disambiguate = $this->getOption('numeric_data_type_disambiguate');
+
         if (!is_array($dataTypes)) {
             $dataTypes = [$dataTypes];
         }
@@ -49,31 +51,45 @@ class NumericPropertySelect extends Select
             $qb->setParameter($index, sprintf('numeric:%s', $dataType));
         }
         $query = $qb->getQuery();
-
         $valueOptions = [];
         foreach ($query->getResult() as $templateProperty) {
             $property = $templateProperty->getProperty();
             $template = $templateProperty->getResourceTemplate();
-            if (!isset($valueOptions[$property->getId()])) {
-                $valueOptions[$property->getId()] = [
-                    'label' => $property->getLabel(),
-                    'value' => $property->getId(),
+            $value = $disambiguate
+                ? sprintf('%s:%s', $templateProperty->getDataType(), $property->getId())
+                : $property->getId();
+            $label = $disambiguate
+                ? sprintf('%s (%s)', $property->getLabel(), $templateProperty->getDataType())
+                : $property->getLabel();
+            if (!isset($valueOptions[$value])) {
+                $valueOptions[$value] = [
+                    'label' => $label,
+                    'value' => $value,
                     'template_labels' => [],
                 ];
             }
+            $templateLabel = $disambiguate
+                ? sprintf(
+                    '• %s: %s',
+                    $template->getLabel(),
+                    $templateProperty->getAlternateLabel() ?: $property->getLabel()
+                )
+                : sprintf(
+                    '• %s: %s (%s)',
+                    $template->getLabel(),
+                    $templateProperty->getAlternateLabel() ?: $property->getLabel(),
+                    $templateProperty->getDataType()
+                );
             // More than one template could use the same property.
-            $valueOptions[$property->getId()]['template_labels'][] = sprintf(
-                '• %s: %s (%s)',
-                $template->getLabel(),
-                $templateProperty->getAlternateLabel() ?: $property->getLabel(),
-                $templateProperty->getDataType()
-            );
+            $valueOptions[$value]['template_labels'][] = $templateLabel;
         }
+
         // Include template/property labels in the option title attribute.
-        foreach ($valueOptions as $propertyId => $option) {
-            $templateLabels = $valueOptions[$propertyId]['template_labels'];
-            $valueOptions[$propertyId]['attributes']['title'] = implode("\n", $templateLabels);
+        foreach ($valueOptions as $value => $option) {
+            $templateLabels = $option['template_labels'];
+            $valueOptions[$value]['attributes']['title'] = implode("\n", $templateLabels);
         }
+
         // Sort options alphabetically.
         usort($valueOptions, function ($a, $b) {
             return strcasecmp($a['label'], $b['label']);
