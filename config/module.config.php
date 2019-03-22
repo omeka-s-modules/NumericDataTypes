e3b325d232923254d59d154512155dc0a7fc256b
Jim Safley <jimsafley@gmail.com>
2019-03-22 10:29:38 -0400

    Add translator config
    Fixes #11

--- config/module.config.php
+++ config/module.config.php
@@ -1,5 +1,15 @@
 <?php
 return [
+    'translator' => [
+        'translation_file_patterns' => [
+            [
+                'type' => 'gettext',
+                'base_dir' => dirname(__DIR__) . '/language',
+                'pattern' => '%s.mo',
+                'text_domain' => null,
+            ],
+        ],
+    ],
     'view_manager' => [
         'template_path_stack' => [
             OMEKA_PATH . '/modules/NumericDataTypes/view',
