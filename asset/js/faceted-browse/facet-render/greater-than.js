bfed06149032a5a1ce31a2bdb737365c5038c1bf
Jim Safley <jimsafley@gmail.com>
2021-05-04 17:39:15 -0400

    Query every input

--- asset/js/faceted-browse/facet-render/greater-than.js
+++ asset/js/faceted-browse/facet-render/greater-than.js
@@ -2,20 +2,16 @@ $(document).ready(function() {
 
 const container = $('#container');
 
-container.on('change', '.greater-than', function(e) {
+container.on('input', '.greater-than', function(e) {
     const thisRange = $(this);
     const facet = thisRange.closest('.facet');
     const facetData = facet.data('facetData');
+    facet.find('.greater-than-reset').show();
+    facet.find('.greater-than-value').text(thisRange.val());
     FacetedBrowse.setFacetQuery(
         facet.data('facetId'),
         `numeric[int][gt][pid]=${facetData.property_id}&numeric[int][gt][val]=${encodeURIComponent(thisRange.val())}`
     );
-});
-container.on('input', '.greater-than', function(e) {
-    const thisRange = $(this);
-    const facet = thisRange.closest('.facet');
-    facet.find('.greater-than-reset').show();
-    facet.find('.greater-than-value').text(thisRange.val());
 });
 container.on('click', '.greater-than-reset', function(e) {
     const thisButton = $(this);
