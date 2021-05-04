9c5b167dc5f9c54e9718b6fce2819d85b05ebabf
Jim Safley <jimsafley@gmail.com>
2021-05-04 17:23:49 -0400

    Show all available values for greater than facet

--- asset/js/faceted-browse/facet-data-form/greater-than.js
+++ asset/js/faceted-browse/facet-data-form/greater-than.js
@@ -23,4 +23,30 @@ FacetedBrowse.registerFacetSetHandler('greater_than', function() {
     }
 });
 
+// Handle show all values.
+$(document).on('click', '#greater-than-show-integer-values', function(e) {
+    const timestampValues = $('#greater-than-integer-values');
+    if (this.checked) {
+        $.get(timestampValues.data('integerValuesUrl'), {
+            property_id: $('#greater-than-property-id').val(),
+            query: $('#category-query').val()
+        }, function(data) {
+            if (data.length) {
+                data.forEach(value => {
+                    timestampValues.append(`<tr><td style="width: 90%; padding: 0; border-bottom: 1px solid #dfdfdf;">${value.value}</td><td style="width: 10%; padding: 0; border-bottom: 1px solid #dfdfdf;">${value.value_count}</td></tr>`);
+                });
+            } else {
+                timestampValues.append(`<tr><td>${Omeka.jsTranslate('There are no available values.')}</td></tr>`);
+            }
+        });
+    } else {
+        timestampValues.empty();
+    }
+});
+// Handle property ID select.
+$(document).on('change', '#greater-than-property-id', function(e) {
+    $('#greater-than-show-integer-values').prop('checked', false);
+    $('#greater-than-integer-values').empty();
+});
+
 });
--- src/Controller/SiteAdmin/FacetedBrowse/IndexController.php
+++ src/Controller/SiteAdmin/FacetedBrowse/IndexController.php
@@ -26,8 +26,6 @@ class IndexController extends AbstractActionController
         // Get the IDs of all items that satisfy the category query.
         $ids = $api->search('items', $query, ['returnScalar' => 'id'])->getContent();
 
-        // Get all unique literal values of the specified property of the
-        // specified items.
         $dql = '
         SELECT v.value value, COUNT(v.value) value_count
         FROM Omeka\Entity\Value v
@@ -38,6 +36,42 @@ class IndexController extends AbstractActionController
         ORDER BY value ASC';
         $query = $em->createQuery($dql)
             ->setParameter('type', 'numeric:timestamp')
+            ->setParameter('propertyId', $propertyId)
+            ->setParameter('ids', $ids);
+        $values = $query->getResult();
+
+        $response = $this->getResponse();
+        $responseHeaders = $response->getHeaders();
+        $responseHeaders->addHeaderLine('Content-Type: application/json');
+        $response->setContent(json_encode($values));
+        return $response;
+    }
+
+    public function integerValuesAction()
+    {
+        $propertyId = $this->params()->fromQuery('property_id');
+        $query = $this->params()->fromQuery('query');
+        parse_str($query, $query);
+        $query['site_id'] = $this->currentSite()->id();
+
+        $api = $this->services->get('Omeka\ApiManager');
+        $em = $this->services->get('Omeka\EntityManager');
+
+        // Get the IDs of all items that satisfy the category query.
+        $ids = $api->search('items', $query, ['returnScalar' => 'id'])->getContent();
+
+        // For ordering to work, we must cast string values to int by forcing a
+        // cast using + 0.
+        $dql = '
+        SELECT v.value + 0 value, COUNT(v.value) value_count
+        FROM Omeka\Entity\Value v
+        WHERE v.type = :type
+        AND v.property = :propertyId
+        AND v.resource IN (:ids)
+        GROUP BY value
+        ORDER BY value ASC';
+        $query = $em->createQuery($dql)
+            ->setParameter('type', 'numeric:integer')
             ->setParameter('propertyId', $propertyId)
             ->setParameter('ids', $ids);
         $values = $query->getResult();
